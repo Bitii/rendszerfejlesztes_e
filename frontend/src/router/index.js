e6ab42ede5636cfd56410ccdbbbb7180671fc073
Tibi <tari.tibor70@gmail.com>
2024-11-10 15:13:06 +0100

    user oldal levédése, csak akkor navigál át, ha van bejelentkezett felhasználó, egyéb esetben login page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Index from '../components/Index.vue';
-import Signup from '../components/Signup.vue';
-import Signin from '../components/Signin.vue';
-import User from '../components/User.vue';
-import MovieDetail from '../components/MovieDetail.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Index from "../components/Index.vue";
+import Signup from "../components/Signup.vue";
+import Signin from "../components/Signin.vue";
+import User from "../components/User.vue";
+import MovieDetail from "../components/MovieDetail.vue";
+import { useUserStore } from "../../stores/user";
 
 const routes = [
-  { path: '/', component: Index },
-  { path: '/signup', component: Signup },
-  { path: '/signin', component: Signin },
-  { path: '/user', component: User },
-  { path: '/movie/:id', name: 'MovieDetail', component: MovieDetail },
+  { path: "/", name: "home", component: Index },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/signin", name: "login", component: Signin },
+  {
+    path: "/user",
+    name: "user",
+    component: User,
+    meta: { requiresAuth: true },
+  },
+  { path: "/movie/:id", name: "MovieDetail", component: MovieDetail },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const userData = useUserStore();
+  if (to.matched.some((record) => record.meta.requiresAuth) && !userData.isLoggedIn)
+  {
+    next({ name: "login" });
+  } else { next(); }
 });
 
 export default router;
--- frontend/stores/user.js
+++ frontend/stores/user.js
@@ -2,4 +2,7 @@ import { defineStore } from "pinia";
 
 export const useUserStore = defineStore('user', {
   state: () => ({ user: {} }),
+  getters: {
+    isLoggedIn: (state) => !!state.user.id, 
+  }
 });
